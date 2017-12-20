a2550f7493649879f589ae617cd8bc49289b6c5c
Lillian Chernin <l.smith1654@gmail.com>
2017-12-19 21:38:35 -0600

    create returnConvertedCurrency function

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-function convertCurrency(event) {
-  if ('type' in event.target && event.target.type === 'button') {
-    console.log('handle currency conversion here');
-  }
+const returnConvertedCurrency = (event) => {
+  let id = event.target.getAttribute("id");
+  let dollars = $('#USD').val();
+  $.get("https://api.fixer.io/latest?base=USD", (response) => {
+    let newCurrency = Math.round(response.rates[id] * dollars * 100) / 100;
+    if (newCurrency >= 0 || newCurrency <= 0) {
+      $('#output_amount').text(newCurrency);
+      $('#output_currency').text(id);
+      $('#USD').val('');
+    } else {
+      alert('Please only enter numbers into form.');
+    }
+  })
 }
 
-document.body.addEventListener('click', convertCurrency);
+$('#EUR').click((event) => {
+  returnConvertedCurrency(event);
+})
+
+$('#GBP').click((event) => {
+  returnConvertedCurrency(event);
+})
+
+$('#CNY').click((event) => {
+  returnConvertedCurrency(event);
+})
+
+$('#JPY').click((event) => {
+  returnConvertedCurrency(event);
+})
